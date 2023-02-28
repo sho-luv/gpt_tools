3fd39ad5a25002d082cc70c111ba4bb83cbf3d9c
sho-luv <sho.luv@gmail.com>
2023-02-27 23:59:38 -0800

    changed resolv_hostname.py to execuate

---
+++ resolve_hostnames.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import argparse
+import socket
+import threading
+
+def resolve_hostname(hostname):
+    try:
+        ip_address = socket.gethostbyname(hostname)
+        print(f"{hostname} resolved to {ip_address}")
+    except socket.error as e:
+        print(f"{hostname} failed to resolve: {e}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Resolve hostnames in parallel")
+    parser.add_argument("filename", help="file containing hostnames, one per line")
+    parser.add_argument("-t", "--threads", type=int, default=10, help="number of threads to use")
+    args = parser.parse_args()
+
+    with open(args.filename) as f:
+        hostnames = f.read().splitlines()
+
+    threads = []
+    for hostname in hostnames:
+        thread = threading.Thread(target=resolve_hostname, args=(hostname,))
+        threads.append(thread)
+
+    for i in range(0, len(threads), args.threads):
+        for thread in threads[i:i+args.threads]:
+            thread.start()
+        for thread in threads[i:i+args.threads]:
+            thread.join()
+
+if __name__ == "__main__":
+    main()
+
