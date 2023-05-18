132575622507e2772876d552d824a542fd5280d5
sho-luv <sho.luv@gmail.com>
2023-05-18 16:15:26 -0700

    added dump usernames

--- checknull.py
+++ checknull.py
@@ -2,32 +2,54 @@
 import argparse
 from impacket.smbconnection import SMBConnection
 from termcolor import colored
+from subprocess import Popen, PIPE
 
 def check_null_session(ip):
     try:
         conn = SMBConnection(ip, ip, timeout=2)
         conn.login('', '')
         conn.logoff()
-        print(colored(f"{ip} allows null sessions", 'yellow'))
+        return colored(f"{ip} allows null sessions", 'yellow')
     except Exception as e:
-        pass
+        return None
+
+def list_users(ip):
+    cmd = ['rpcclient', '-U', '', '-N', ip, '-c', 'enumdomusers']
+    p1 = Popen(cmd, stdout=PIPE)
+    p2 = Popen(['cut', '-d[', '-f2'], stdin=p1.stdout, stdout=PIPE)
+    p3 = Popen(['cut', '-d]', '-f1'], stdin=p2.stdout, stdout=PIPE)
+    p4 = Popen(['sort', '-u'], stdin=p3.stdout, stdout=PIPE)
+    output = p4.communicate()[0]
+    return output.decode()
 
 def main():
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-f', '--file', help='Path to a file containing IP addresses (one per line)')
-    group.add_argument('-i', '--ip', nargs='+', help='List of IP addresses to check for null sessions')
+    parser.add_argument('input', help='IP address or path to a file containing IP addresses (one per line)')
+    parser.add_argument('-l', '--list', action='store_true', help='List users of the given IP or IPs')
+    parser.add_argument('-f', '--file', help='Output file')
     args = parser.parse_args()
 
     ips = []
-    if args.file:
-        with open(args.file, 'r') as f:
+    try:
+        with open(args.input, 'r') as f:
             ips = [line.strip() for line in f]
-    else:
-        ips = args.ip
+    except FileNotFoundError:
+        ips = [args.input]
 
+    results = []
     for ip in ips:
-        check_null_session(ip)
+        result = check_null_session(ip)
+        if result is not None:
+            results.append(result)
+            if args.list:
+                users = list_users(ip)
+                results.append(f"Users for {ip}:\n{users}")
+
+    if args.file:
+        with open(args.file, 'w') as f:
+            f.write('\n'.join(results))
+    else:
+        print('\n'.join(results))
 
 if __name__ == '__main__':
     main()
